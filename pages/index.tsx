f219a1527aa044bb6c0ea0ab72497ae4a1ff3215
Steven <setienne97231@gmail.com>
2023-11-23 12:02:12 -0400

    Try cleaning default template

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import "@/app/global.css";
+
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <title>Home</title>
+      </main>
+      <h1>Home</h1>
+    </>
+  );
+}
