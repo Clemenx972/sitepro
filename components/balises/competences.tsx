54e9c41917d361d5050df00b06ff6606173e67f8
Steven <setienne97231@gmail.com>
2023-12-06 20:08:21 -0400

    Alignement compétences
    Effectué (rajout couleur pour préférences à faire)

--- components/balises/competences.tsx
+++ components/balises/competences.tsx
@@ -5,10 +5,10 @@ const Competences = () => {
     <div className="hidden sm:block w-full">
         <div className="w-full">
             <h4 className='text-2xl mb-6 sm:mb-10 text-4x1 text-center font-extrabold '>Compétences</h4>
-            <div className='flex justify-between'>
-                <div className='flex justify-between'>
-                    <h5 className='text-xl h-4 ml-16 font-bold'>Design</h5>
-                        <ul className=' flex-col gap-2 mt-10'>
+            <div className='flex justify-center'>
+                <div className='flex flex-col items-start pl-44 mr-48'>
+                    <h5 className='text-xl h-4 font-bold'>Design</h5>
+                        <ul className='flex flex-col items-start gap-2 mt-10'>
                                 <li className='flex items-center gap-1 text-lg '>
                                     <span>
                                     <img src='/images/icones/css.png' alt="CSS" height={30} width={20}/>
@@ -24,9 +24,10 @@ const Competences = () => {
                                     
                                     <span className='text-1xl'>tailwind</span>                           
                                 </li>
-
-
                         </ul>
+                </div>
+                <div className='flex flex-col items-start mr-48'>
+                        
                     <h5 className='text-xl h-4 font-bold'>Front-end</h5>
                         <ul className='flex flex-col gap-2 mt-10'>
                                 <li className='flex items-center gap-1 text-lg'>
@@ -61,7 +62,8 @@ const Competences = () => {
                                     <span className='text-1xl'>TypeScript</span>                           
                                 </li>
                         </ul>
-
+                    </div>
+                    <div className='flex flex-col items-start mr-48'>
                         <h5 className='text-xl h-4 font-bold'>Back-end</h5>
                         <ul className='flex flex-col gap-2 mt-10'>
                                 <li className='flex items-center gap-1 text-lg'>
@@ -90,9 +92,9 @@ const Competences = () => {
 
                                 
                         </ul>
+                    </div>
 
-
-
+                    <div className='flex flex-col items-start mr-48'>
                         <h5 className='text-xl h-4 font-bold'>Autre</h5>
                         <ul className='flex flex-col gap-2 mt-10'>
                                 <li className='flex items-center gap-1 text-lg'>
@@ -113,6 +115,7 @@ const Competences = () => {
 
                                
                         </ul>
+                    </div>
                 </div>
 
 
@@ -120,7 +123,6 @@ const Competences = () => {
 
         </div>
 
-    </div>
 
     </>
   )
