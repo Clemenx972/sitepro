396d6b453241f8e4f6c678229be447042e9afb80
Steven <setienne97231@gmail.com>
2024-03-21 22:31:43 -0400

    Logo site

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,31 @@
-import '@/app/globals.css';
+import "@/app/globals.css";
 
-import bons_plans from '@/components/balises/bons_plans-section';
-import Navbar_Section from '@/components/balises/navbar-section';
-import Footer_Section from '@/components/balises/footer-section';
-import Presentation from '@/components/balises/presentation';
-import Competences from '@/components/balises/competences';
-import Projets from '@/components/balises/projets';
-import Experiences from '@/components/balises/experiences';
-import Contact from '@/components/balises/contact';
+import bons_plans from "@/components/balises/bons_plans-section";
+import Navbar_Section from "@/components/balises/navbar-section";
+import Footer_Section from "@/components/balises/footer-section";
+import Presentation from "@/components/balises/presentation";
+import Competences from "@/components/balises/competences";
+import Projets from "@/components/balises/projets";
+import Experiences from "@/components/balises/experiences";
+import Contact from "@/components/balises/contact";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
-      <header>
+      <Head>
         <title>Mon site</title>
-      </header>
+        <link rel='icon' type='image/jpg' href='/logo_cyber_2.jpg' />
+      </Head>
 
-       <Navbar_Section />
+      <Navbar_Section />
       <Presentation />
       <Competences />
       <Projets />
       <Experiences />
-      <Contact/>
+      <Contact />
       <div></div>
-    <Footer_Section />
+      <Footer_Section />
     </>
   );
 }
