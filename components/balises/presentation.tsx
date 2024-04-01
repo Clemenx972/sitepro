f25f518b08c66e83ac62f5031c383bdb12e42f71
Steven <setienne97231@gmail.com>
2024-03-31 23:19:58 -0400

    Update presentation.tsx

--- components/balises/presentation.tsx
+++ components/balises/presentation.tsx
@@ -2,14 +2,17 @@ import React from "react";
 const Presentation = () => {
   return (
     <section className='lg:py-16'>
-      <div className='grid grid-cols-1 sm:grid-cols-12'>
+      <div className='grid grid-cols-2 sm:grid-cols-12'>
         <div
           className='col-span-6 place-self-center text-center sm:text-left justify-self-center'
           style={{ transform: "none" }}
         >
           <h1 className='text-7xl font-bold'>Steven Etienne</h1>
-          <h2 className='text-3xl font-semibold'>Etudiant Cybersécurité </h2>
-          <h3 className='mt-12'>
+          <h2 className='text-3xl font-semibold mt-4'>
+            Etudiant Cybersécurité{" "}
+          </h2>
+
+          <h3 className='mt-10'>
             {" "}
             <button>
               <a href='./CV_STEVEN_ETIENNE.pdf' download>
@@ -23,6 +26,13 @@ const Presentation = () => {
             </button>
           </h3>
           {/* ajoute une photo sale chien */}
+        </div>
+        <div className='text-xl col-span-6 place-self-center text-justify justify-self-center sm:text-justify mt-4'>
+          Salut !<br></br> Je suis un jeune étudiant passioné d'informatique qui
+          s'engage quotidiennement dans la découverte de nouvelles pratiques,
+          quelles soient axées sur du développement web ou applicatif. Je porte
+          également un intérêt au développement personnel afin de devenir chaque
+          jour meilleur que le prércédent.
         </div>
       </div>
     </section>
