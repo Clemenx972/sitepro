ae56fd0bbb071c5407238894a7783cbe8c5242a9
Steven <setienne97231@gmail.com>
2024-08-07 20:57:41 -0400

    feat: webform changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import { Metadata } from "next";
 import "./globals.css";
 import ToggleButton from "@/components/toggleButton";
 import NavigationBar from "@/components/navBar";
-
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 export const metadata: Metadata = {
   title: "Porfolio",
   description: "Porfolio de Steven Etienne",
@@ -16,6 +17,7 @@ export default async function RootLayout({
       <body className='flex min-h-screen flex-col bg-darkpurple'>
         <NavigationBar />
         {children}
+      <ToastContainer />
       </body>
     </html>
   );
--- src/components/balises/contact.tsx
+++ src/components/balises/contact.tsx
@@ -1,5 +1,6 @@
 import colorToggle from "@/components/colorTogle";
 import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 const Contact = () => {
   const [colorState, setColorState] = useState(colorToggle.state);
@@ -9,6 +10,40 @@ const Contact = () => {
     }, 100);
     return () => clearInterval(interval);
   }, []);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [entry, setEntry] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+    fetch("https://www.formbackend.com/f/5e76ddb8a85be691", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({ name, email, entry }),
+    })
+    .then((response) => {
+      if (response.status === 422) {
+        throw new Error("Validation error");
+      } else if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      return response.json();
+    })
+    .then(data => {
+      // You can even use `data` here. Access `data.submission_text`, `data.values` etc.
+
+      toast.success("Form submitted successfully");
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+  }
+
   return (
     <div>
       {colorState ? (
@@ -16,37 +51,21 @@ const Contact = () => {
           <h4 className='mt-4 mb-2 m:mb-10 font-extrabold text-2xl text-secondary text-center'>
             Me contacter
           </h4>
-          <form
-            action='https://api.web3forms.com/submit'
-            method='POST'
-            id='contact'
-            className='text-center w-full'
-          >
+          <form onSubmit={handleSubmit}>
             <div>
               <div>
-                <input type='hidden' name='access_key' value='MY_MAIL_KEY' />
                 <div className='justify-center pt-4 grid'>
-                  <label className='text-secondary'>Votre Nom</label>
-                  <input
-                    type='text'
-                    name='sujet'
-                    placeholder='Etienne Steven'
-                    className=' w-full h-full sm:w-[500px] sm:h-[50px] flex-col gap-5  font-medium pl-[5px] rounded-[50px] border-2 border-gray-800 hover:border-darksecondary mb-4 rounded-lg;'
-                    required
-                  />
+                <label htmlFor="name" className="text-secondary text-center">Votre Nom</label>
+                <input className='text-secondary  w-full h-full sm:w-[500px] sm:h-[50px] flex-col gap-5  font-medium pl-[5px] rounded-[50px] border-2 border-gray-800 hover:border-darksecondary mb-4 rounded-lg;'  placeholder='Etienne Steven' type="text" id="name" name="name" required onChange={(e) => setName(e.target.value)}/>
                 </div>
                 <div className='pt-2 justify-center grid'>
-                  <label className='text-secondary'>Votre adresse mail</label>
-                  <input
-                    type='email'
-                    name='email'
-                    placeholder='prenom.nom@gmail.com'
-                    className='flex-col gap-5 w-full h-full sm:w-[500px] sm:h-[50px] mb-4 font-medium pl-[5px] rounded-[50px] border-2 border-gray-800 hover:border-darksecondary rounded-lg;'
-                    required
-                  />
+                <label htmlFor="email" className='text-secondary text-center'>Votre adresse mail</label>
+                <input type="email" id="email" name="email"
+                    placeholder='prenom.nom@gmail.com' className='flex-col gap-5 w-full h-full sm:w-[500px] sm:h-[50px] mb-4 font-medium pl-[5px] rounded-[50px] border-2 border-gray-800 hover:border-darksecondary rounded-lg;'
+                     required onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 <div className='pt-4 justify-center grid'>
-                  <label className='text-secondary'>Votre Sujet</label>
+                  <label className='text-secondary text-center'>Votre Sujet</label>
                   <input
                     type='text'
                     name='sujet'
@@ -56,12 +75,14 @@ const Contact = () => {
                   />
                 </div>
                 <div className='justify-center grid'>
-                  <label className='text-secondary'>Votre message</label>
+                  <label className='text-secondary text-center'>Votre message</label>
                   <textarea
-                    name='message'
+                    name='entry'
+                    id="entry"
                     placeholder='Salut !'
                     className='pt-3 gap-5 w-full h-full sm:w-[500px] sm:h-[50px] font-medium pl-[5px] rounded-[50px] border-2 border-gray-800 hover:border-darksecondary rounded-lg ;'
                     required
+                    onChange={(e) => setEntry(e.target.value)}
                   ></textarea>
                 </div>
                 <div className='ml-4 pt-4 grid justify-center sm:justify-left'>
@@ -81,52 +102,38 @@ const Contact = () => {
           <h4 className='mt-4 mb-2 m:mb-10 font-extrabold text-2xl text-coral text-center'>
             Me contacter
           </h4>
-          <form
-            action='https://api.web3forms.com/submit'
-            method='POST'
-            id='contact'
-            className='text-center w-full'
-          >
+          <form onSubmit={handleSubmit}>
             <div>
               <div>
-                <input type='hidden' name='access_key' value='MY_MAIL_KEY' />
                 <div className='justify-center pt-4 grid'>
-                  <label className=''>Votre Nom</label>
-                  <input
-                    type='text'
-                    name='sujet'
-                    placeholder='Etienne Steven'
-                    className=' w-full h-full sm:w-[500px] sm:h-[50px] flex-col gap-5  font-medium pl-[5px] rounded-[50px] border-2 border-gray-800 hover:border-primary mb-4 rounded-lg;'
-                    required
-                  />
+                  <label htmlFor="name" className="text-center">Votre Nom</label>
+                  <input className='text-coral  w-full h-full sm:w-[500px] sm:h-[50px] flex-col gap-5  font-medium pl-[5px] rounded-[50px] border-2 border-gray-800 hover:border-primary mb-4 rounded-lg;'  placeholder='Etienne Steven' type="text" id="name" name="name" required onChange={(e) => setName(e.target.value)}/>
                 </div>
                 <div className='pt-2 justify-center grid'>
-                  <label className=''>Votre adresse mail</label>
-                  <input
-                    type='email'
-                    name='email'
-                    placeholder='prenom.nom@gmail.com'
-                    className='flex-col gap-5 w-full h-full sm:w-[500px] sm:h-[50px] mb-4 font-medium pl-[5px] rounded-[50px] border-2 border-gray-800 hover:border-primary rounded-lg;'
-                    required
-                  />
+                  <label htmlFor="email" className='text-coral text-center'>Votre adresse mail</label>
+                  <input type="email" id="email" name="email"
+                      placeholder='prenom.nom@gmail.com' className='flex-col gap-5 w-full h-full sm:w-[500px] sm:h-[50px] mb-4 font-medium pl-[5px] rounded-[50px] border-2 border-gray-800 hover:border-primary rounded-lg;'
+                      required onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 <div className='pt-4 justify-center grid'>
-                  <label className=''>Votre Sujet</label>
-                  <input
-                    type='text'
-                    name='sujet'
-                    placeholder='Recrutement, Projet, Autre...'
-                    className='flex-col gap-5 w-full h-full sm:w-[500px] sm:h-[50px] font-medium pl-[5px] rounded-[50px] bg-gray-[#DCDCDC] border-2 border-gray-800 hover:border-primary mb-4 rounded-lg; block;'
-                    required
-                  />
+                    <label className='text-coral text-center'>Votre Sujet</label>
+                    <input
+                      type='text'
+                      name='sujet'
+                      placeholder='Recrutement, Projet, Autre...'
+                      className='flex-col gap-5 w-full h-full sm:w-[500px] sm:h-[50px] font-medium pl-[5px] rounded-[50px] bg-gray-[#DCDCDC] border-2 border-gray-800 hover:border-primary mb-4 rounded-lg; block;'
+                      required
+                    />
                 </div>
                 <div className='justify-center grid'>
-                  <label className=''>Votre message</label>
+                  <label className='text-coral text-center'>Votre message</label>
                   <textarea
-                    name='message'
+                    name='entry'
+                    id="entry"
                     placeholder='Salut !'
                     className='pt-3 gap-5 w-full h-full sm:w-[500px] sm:h-[50px] font-medium pl-[5px] rounded-[50px] border-2 border-gray-800 hover:border-primary rounded-lg ;'
                     required
+                    onChange={(e) => setEntry(e.target.value)}
                   ></textarea>
                 </div>
                 <div className='ml-4 pt-4 grid justify-center sm:justify-left'>
